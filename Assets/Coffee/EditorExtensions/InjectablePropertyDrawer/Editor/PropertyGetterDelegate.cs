8566095d2bf7908436c72668c001e34255a0c6b2
mob-sakai <sakai@mobcast.jp>
2018-06-18 21:07:34 +0900

    fix #13; Error has occurred with UnityEvent collection such as List<UnityEvent> and UnityEvent[].

--- Assets/Coffee/EditorExtensions/InjectablePropertyDrawer/Editor/PropertyGetterDelegate.cs
+++ Assets/Coffee/EditorExtensions/InjectablePropertyDrawer/Editor/PropertyGetterDelegate.cs
@@ -11,7 +11,7 @@ namespace Coffee.EditorExtensions
 	{
 		const BindingFlags kBfAll = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy;
 		static readonly Dictionary<Type,Dictionary<string, MemberInfo>> s_TypeMemberMap = new Dictionary<Type,Dictionary<string, MemberInfo>>();
-		static readonly Regex s_RegexArray = new Regex(@"^Array\.data\[([0-9]+)\]\.!?", RegexOptions.Compiled);
+		static readonly Regex s_RegexArray = new Regex(@"^Array\.data\[([0-9]+)\](\.!?|$)", RegexOptions.Compiled);
 		static readonly Regex s_RegexNested = new Regex(@"^(\w+)\.!?", RegexOptions.Compiled);
 		static readonly Dictionary<int,Func<object,object>> s_PropertyGetterMap = new Dictionary<int, Func<object, object>>();
 
@@ -20,51 +20,58 @@ namespace Coffee.EditorExtensions
 		/// </summary>
 		public static object GetInstance(this SerializedProperty property)
 		{
-			int hash = property.propertyPath.GetHashCode();
-
-			// Find getter delegate.
-			Func<object,object> getter = null;
-			if (s_PropertyGetterMap.TryGetValue(hash, out getter))
+			try
 			{
+				int hash = property.propertyPath.GetHashCode();
+
+				// Find getter delegate.
+				Func<object,object> getter = null;
+				if (s_PropertyGetterMap.TryGetValue(hash, out getter))
+				{
+					return getter(property);
+				}
+
+				// Create getter delegate.
+				object instance;
+				string path = property.propertyPath;
+				getter = p => (p as SerializedProperty).serializedObject.targetObject;
+				while (0 < path.Length)
+				{
+					var preGetter = getter;
+					instance = preGetter(property);
+					Match match;
+
+					// Array property.
+					if ((match = s_RegexArray.Match(path)).Success)
+					{
+						var arg = new object[]{ int.Parse(match.Groups[1].Value) };
+						var mi = instance.GetMemberInfo(instance is Array ? "Get" : "get_Item", MemberTypes.Method) as MethodInfo;
+						getter = x => mi.Invoke(preGetter(x), arg);
+						path = s_RegexArray.Replace(path, "");
+					}
+					// Nested property.
+					else if ((match = s_RegexNested.Match(path)).Success)
+					{
+						var fi = instance.GetMemberInfo(match.Groups[1].Value, MemberTypes.Field) as FieldInfo;
+						getter = x => fi.GetValue(preGetter(x));
+						path = s_RegexNested.Replace(path, "");
+					}
+					// Property.
+					else
+					{
+						var fi = instance.GetMemberInfo(path, MemberTypes.Field) as FieldInfo;
+						getter = x => fi.GetValue(preGetter(x));
+						break;
+					}
+				}
+				s_PropertyGetterMap.Add(hash, getter);
+
 				return getter(property);
 			}
-
-			// Create getter delegate.
-			object instance;
-			string path = property.propertyPath;
-			getter = p => (p as SerializedProperty).serializedObject.targetObject;
-			while (0 < path.Length)
+			catch
 			{
-				var preGetter = getter;
-				instance = preGetter(property);
-				Match match;
-
-				// Array property.
-				if ((match = s_RegexArray.Match(path)).Success)
-				{
-					var arg = new object[]{ int.Parse(match.Groups[1].Value) };
-					var mi = instance.GetMemberInfo("get_Item", MemberTypes.Method) as MethodInfo;
-					getter = x => mi.Invoke(preGetter(x), arg);
-					path = s_RegexArray.Replace(path, "");
-				}
-				// Nested property.
-				else if ((match = s_RegexNested.Match(path)).Success)
-				{
-					var fi = instance.GetMemberInfo(match.Groups[1].Value, MemberTypes.Field) as FieldInfo;
-					getter = x => fi.GetValue(preGetter(x));
-					path = s_RegexNested.Replace(path, "");
-				}
-				// Property.
-				else
-				{
-					var fi = instance.GetMemberInfo(path, MemberTypes.Field) as FieldInfo;
-					getter = x => fi.GetValue(preGetter(x));
-					break;
-				}
+				return null;
 			}
-			s_PropertyGetterMap.Add(hash, getter);
-
-			return getter(property);
 		}
 
 		/// <summary>
