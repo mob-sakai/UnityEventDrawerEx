f6b7b69781be0915d32f7862be8d19f00b5cb988
mob-sakai <12690315+mob-sakai@users.noreply.github.com>
2024-07-21 16:22:33 +0900

    add demo

---
+++ Assets/Demo/Demo.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Coffee.EditorExtensions
+{
+    [RequireComponent(typeof(Button))]
+    [ExecuteAlways]
+    public class Demo : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            if (TryGetComponent(out Button button))
+            {
+                button.onClick.AddListener(OnClick_Instance);
+                button.onClick.AddListener(OnClick_Static);
+                button.onClick.AddListener(() => Debug.Log("Hello (ramda)"));
+            }
+
+            if (TryGetComponent(out EventTrigger trigger))
+            {
+                var ev = trigger.triggers.Find(x => x.eventID == EventTriggerType.PointerClick)?.callback;
+                ev?.AddListener(OnClick_Instance);
+                ev?.AddListener(OnClick_Static);
+                ev?.AddListener(_ => Debug.Log("Hello (ramda)"));
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (TryGetComponent(out Button button))
+            {
+                button.onClick.RemoveAllListeners();
+            }
+
+            if (TryGetComponent(out EventTrigger trigger))
+            {
+                var ev = trigger.triggers.Find(x => x.eventID == EventTriggerType.PointerClick)?.callback;
+                ev?.RemoveAllListeners();
+            }
+        }
+
+        private void Register(UnityEvent ev)
+        {
+            ev.AddListener(OnClick_Instance);
+            ev.AddListener(OnClick_Static);
+            ev.AddListener(() => Debug.Log("Hello (ramda)"));
+        }
+
+        private void Unregister(UnityEvent ev)
+        {
+        }
+
+        private void OnClick_Instance()
+        {
+            Debug.Log("Hello (instance)");
+        }
+
+
+        private void OnClick_Instance(BaseEventData ev)
+        {
+            Debug.Log("Hello (instance)");
+        }
+
+        private static void OnClick_Static()
+        {
+            Debug.Log("Hello (static)");
+        }
+
+        private static void OnClick_Static(BaseEventData ev)
+        {
+            Debug.Log("Hello (static)");
+        }
+    }
+}
