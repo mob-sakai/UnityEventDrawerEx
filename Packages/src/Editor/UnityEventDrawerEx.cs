1da0d4659a7d4de6ff437a4eeabc3fb45165750f
mob-sakai <12690315+mob-sakai@users.noreply.github.com>
2024-07-22 10:14:09 +0900

    fix: support Unity 2022.3.23+/2023.2.15+
    Type `UnityEditor.ScriptAttributeUtility.DrawerKeySet` is removed.
    close #18

--- Packages/src/Editor/UnityEventDrawerEx.cs
+++ Packages/src/Editor/UnityEventDrawerEx.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿#if !UNITY_2022_3_OR_NEWER
+#elif UNITY_2022_3_0 || UNITY_2022_3_1 || UNITY_2022_3_2 || UNITY_2022_3_3 || UNITY_2022_3_4 || UNITY_2022_3_5 || UNITY_2022_3_6 || UNITY_2022_3_7 || UNITY_2022_3_8 || UNITY_2022_3_9
+#elif UNITY_2022_3_10 || UNITY_2022_3_11 || UNITY_2022_3_12 || UNITY_2022_3_13 || UNITY_2022_3_14 || UNITY_2022_3_15 || UNITY_2022_3_16 || UNITY_2022_3_17 || UNITY_2022_3_18 || UNITY_2022_3_19
+#elif UNITY_2022_3_20 || UNITY_2022_3_21 || UNITY_2022_3_22
+#elif UNITY_2023_1
+#elif UNITY_2023_2_0 || UNITY_2023_2_1 || UNITY_2023_2_2 || UNITY_2023_2_3 || UNITY_2023_2_4 || UNITY_2023_2_5 || UNITY_2023_2_6 || UNITY_2023_2_7 || UNITY_2023_2_8 || UNITY_2023_2_9
+#elif UNITY_2023_2_10 || UNITY_2023_2_11 || UNITY_2023_2_12 || UNITY_2023_2_13 || UNITY_2023_2_14
+#else
+#define CUSTOM_PROPERTY_DRAWER_V2
+#endif
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -9,7 +20,11 @@ using UnityEngine.Events;
 
 namespace Coffee.EditorExtensions
 {
+#if CUSTOM_PROPERTY_DRAWER_V2
+    [CustomPropertyDrawer(typeof(UnityEventBase), true)]
+#else
     [InjectablePropertyDrawer(typeof(UnityEventBase), true)]
+#endif
     public class UnityEventDrawerEx : UnityEventDrawer
     {
         private const BindingFlags kBfAll = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy;
